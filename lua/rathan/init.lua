1b698701dea6d4c24e5ed9e98cd2f742ec9037cd
RAthan_rithik <rithikrathan.c@gmail.com>
2024-11-23 08:15:28 +0530

    added a lot of plugins and stuffs no LSP yet

--- lua/rathan/init.lua
+++ lua/rathan/init.lua
@@ -1,7 +1,6 @@
--- YES I dont understand it either but i figured it out by myself anyway
+--  YES I dont understand it either but i figured it out by myself anyway
 
 require("rathan.lazy")
-require("rathan.nvimconfig")
 require("rathan.remap")
 
 -- Behold! the unecessary ASCII art
@@ -24,98 +23,142 @@ local rathan = [[
 /    ##     #     ###   ##     ##     ##    ##   ###   ##   ###   ### 
 #                                           /                         
  ##                                        /                          
-                                          /                           
-                                         /                            
+                                          /           
+           
+
 ]]
 
 print(rathan)
 
+--general vim configurations
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.scrolloff = 8
+vim.opt.cursorline = true
+vim.g.have_nerd_font = false
+vim.opt.undofile = true
+vim.opt.expandtab = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.list = true
+vim.opt.listchars = {tab = '» ', trail = ".", nbsp = "␣"}
 
 
 --colorscheme configuration
 
 local colorScheme = require("darkvoid")
 colorScheme.setup({
-    transparent = true,
-    glow = true,
-    show_end_of_buffer = true,
+	transparent = true,
+	glow = true,
+	show_end_of_buffer = true,
 
-    colors = {
-        fg = "#c0c0c0",
-        bg = "#1c1c1c",
-        cursor = "#bdfe58",
-        line_nr = "#404040",
-        visual = "#303030",
-        comment = "#585858",
-        string = "#d1d1d1",
-        func = "#e1e1e1",
-        kw = "#f1f1f1",
-        identifier = "#b1b1b1",
-        type = "#a1a1a1",
-        type_builtin = "#c5c5c5", -- current
-        -- type_builtin = "#8cf8f7", -- glowy blue old (was present by default before type_builtin was introduced added here for people who may like it)
-        search_highlight = "#1bfd9c",
-        operator = "#1bfd9c",
-        bracket = "#e6e6e6",
-        preprocessor = "#4b8902",
-        bool = "#66b2b2",
-        constant = "#b2d8d8",
+	colors = {
+		fg = "#c0c0c0",
+		bg = "#0f0f0f",
+		cursor = "#e07b7b",
+		line_nr = "#ff1e00",
+		visual = "#303030",
+		comment = "#585858",
+		string = "#d4b2ab",
+		func = "#ff6347",
+		kw = "#ff5555",
+		identifier = "#b1b1b1",
+		type = "#a1a1a1",
+		type_builtin = "#c5c5c5", -- current
+		--type_builtin = "#8cf8f7", -- glowy blue old (was present by default before type_builtin was introduced added here for people who may like it)
+		search_highlight = "#fd571b",
+		operator = "#d63e3e",
+		bracket = "#e6e6e6",
+		preprocessor = "#4b8902",
+		bool = "#ffa07a",
+		constant = "#f59064",
 
-        -- enable or disable specific plugin highlights
-        plugins = {
-            gitsigns = true,
-            nvim_cmp = true,
-            treesitter = true,
-            nvimtree = true,
-            telescope = true,
-            lualine = true,
-            bufferline = true,
-            oil = true,
-            whichkey = true,
-            nvim_notify = true,
-        },
+		-- enable or disable specific plugin highlights
+		plugins = {
+			gitsigns = true,
+			nvim_cmp = true,
+			treesitter = true,
+			nvimtree = true,
+			telescope = true,
+			lualine = true,
+			bufferline = true,
+			oil = true,
+			whichkey = true,
+			nvim_notify = true,
+		},
 
-        -- gitsigns colors
-        added = "#baffc9",
-        changed = "#ffffba",
-        removed = "#ffb3ba",
+		-- gitsigns colors
+		added = "#baffc9",
+		changed = "#ffffba",
+		removed = "#ffb3ba",
 
-        -- Pmenu colors
-        pmenu_bg = "#1c1c1c",
-        pmenu_sel_bg = "#1bfd9c",
-        pmenu_fg = "#c0c0c0",
+		-- Pmenu colors
+		pmenu_bg = "#1c1c1c",
+		pmenu_sel_bg = "#fa3e19",
+		pmenu_fg = "#fc6142",
 
-        -- EndOfBuffer color
-        eob = "#3c3c3c",
+		-- EndOfBuffer color
+		eob = "#3c3c3c",
 
-        -- Telescope specific colors
-        border = "#585858",
-        title = "#bdfe58",
+		-- Telescope specific colors
+		border = "#ff1e00",
+		title = "#ff1e00",
 
-        -- bufferline specific colors
-        bufferline_selection = "#1bfd9c",
+		-- bufferline specific colors
+		bufferline_selection = "#fd7d1b",
 
-        -- LSP diagnostics colors
-        error = "#dea6a0",
-        warning = "#d6efd8",
-        hint = "#bedc74",
-        info = "#7fa1c3",
-    },
+		-- LSP diagnostics colors
+		error = "#ff0000",
+		warning = "#ff6a00",
+		hint = "#e600ff",
+		info = "#00ff6a",
+	},
 })
 
 vim.cmd("colorscheme darkvoid")
---vim.api.nvim_set_hl(0,"Normal", {bg = "none"})
---vim.api.nvim_set_hl(0,"NormalFloat", {bg = "none"})
+
 
 --Treesitter configurations
+
 local treesitter = require('nvim-treesitter.configs')
 treesitter.setup ({
-  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "cpp", "java" },
-  sync_install = false,
-  auto_install = true,
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
+	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "python", "cpp", "java" },
+	sync_install = false,
+	auto_install = true,
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
 })
+
+--Harpoon with telescope configurations
+local harpoon = require('harpoon')
+harpoon:setup({})
+
+local conf = require("telescope.config").values
+local function toggle_telescope(harpoon_files)
+	local file_paths = {}
+	for _, item in ipairs(harpoon_files.items) do
+		table.insert(file_paths, item.value)
+	end
+
+	require("telescope.pickers").new({}, {
+		prompt_title = "Harpoon",
+		finder = require("telescope.finders").new_table({
+			results = file_paths,
+		}),
+		previewer = conf.file_previewer({}),
+		sorter = conf.generic_sorter({}),
+	}):find()
+end
+
+vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,{ desc = "Open harpoon window" })
+
+--UndoTree configuration
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
--- lua/rathan/remap.lua
+++ lua/rathan/remap.lua
@@ -1,16 +1,26 @@
 vim.opt.mouse =  "a"
 vim.opt.timeoutlen = 300
 
+--Plugin specific keymaps
+vim.keymap.set("n","<leader>gs", vim.cmd.Git)
 
+
+--godlike keymaps
 vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }) 
+vim.keymap.set('i', '<leader><Tab>', '<Esc>', { noremap = true, silent = true, desc = 'Exit insert mode' })
+vim.keymap.set('v', '<leader><Tab>', '<Esc>', { noremap = true, silent = true, desc = 'Exit visual mode' })
+vim.keymap.set('t', '<leader><Tab>', '<C-\\><C-n>', { noremap = true, silent = true, desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<leader>d', 'yyp', { desc = 'Duplicate current line' })
 
---Split window navigations keymaps CTRL+<hjkl>
+--Split windows, navigations keymaps CTRL+<hjkl>
+
+vim.keymap.set('n', '<leader>h', ':split<CR>', {desc = 'Split horizontal windowx'})
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>', {desc = 'Split vertical windowx'})
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-   
+
 --Telescope keymaps
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
@@ -21,3 +31,18 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help ta
 vim.keymap.set('n', '<leader>fs',function() 
 	builtin.grep_string({ search = vim.fn.input("Grep >")}); 
 end, {desc = 'Telescope grep'})
+
+--Harpoon with telescope setup and keymaps
+local harpoon = require("harpoon")
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<leader>s", function() harpoon:list():remove() end)
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+--Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
